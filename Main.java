6339859d0fa54aa2da8e6f05a1d93c7b61df6281
Aya <yassienaya714@gmail.com>
2024-12-20 19:03:48 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        firstFrame s = new firstFrame();
+        try {
+            Thread.sleep(2000);
+            s.dispose();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        SignFrame signFrame = new SignFrame();
+    }
+}
